cb4a3f834936546f4253057066fcd2234e55b259
Hari Adhikari <harry_ac07@yahoo.com>
2017-04-13 22:33:00 +0300

    Added Form and Completed adding data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import data from '../public/javascripts/data.js'
 import AptList from './AptList'
+import AddAppointment from './AddAppointment'
 import _ from 'lodash'
 // import axios from 'axios';
 
@@ -9,9 +10,12 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            myAppointments: []
+            myAppointments: [],
+            aptBodyVisible : false
         };
         this.deleteMessage=this.deleteMessage.bind(this);
+        this.toggleAddDisplay = this.toggleAddDisplay.bind(this);
+        this.addItem = this.addItem.bind(this);
     }
 
     componentWillMount() {
@@ -35,8 +39,22 @@ class App extends Component {
         });
     } // deleteMessage
 
+    toggleAddDisplay() {
+        var tempVisibility = !this.state.aptBodyVisible;
+        this.setState({
+            aptBodyVisible : tempVisibility
+        })
+    } //toggleAddDisplay
+
+    addItem(tempItem){
+        var tempApts = this.state.myAppointments;
+        // tempApts.push(tempItem);
+        this.setState({
+            myAppointments :[...tempApts,tempItem]
+        })
+    }//addItem
+
     render() {
-        console.log(this.state.myAppointments);
         var filteredApts=this.state.myAppointments.map(function(item,i){
             return(
                 <AptList key={i} singleItem={item} whichItem={item} onDelete={this.deleteMessage}/>
@@ -44,6 +62,7 @@ class App extends Component {
         }.bind(this));
         return (
             <div className="interface">
+                <AddAppointment bodyVisible={this.state.aptBodyVisible} handleToggle={this.toggleAddDisplay} addApt={this.addItem} />
                 <ul className="item-list media-list">
                     {filteredApts}
                 </ul>
---
+++ src/SearchAppointments.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+class SearchAppointments extends React.Component {
+
+}
+export default SearchAppointments;
